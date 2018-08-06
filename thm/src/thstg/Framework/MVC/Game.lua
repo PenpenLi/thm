ee5432f73b92a6f5a6b386971150e8e4e063be4c
cnscj <cnscj@qq.com>
2018-08-06 16:43:03 +0800

    fix:搜索路径

--- thm/src/thstg/Framework/MVC/Game.lua
+++ thm/src/thstg/Framework/MVC/Game.lua
@@ -14,18 +14,20 @@ function M:createScene()
 end
 
 function M:createEnv(gameRoot)
+    --添加LUA包含路径
+    local newPath = string.gsub(gameRoot, "%.", "/")    --转移
+    package.path = package.path .. ';' .. newPath .. '/?.lua'
+
+    --添加资源路径
+    cc.FileUtils:getInstance():addSearchPath("src/" .. newPath .. "/")
+
+    --子类初始化
     return self:onEnv(gameRoot)
 
 end
 
 --[[以下函数由子类重载]]--
 function M:onEnv(gameRoot)  
-    --添加LUA包含路径
-    local newPath = string.gsub(gameRoot, ".", "/") 
-    package.path = package.path .. ';' .. newPath .. '/?.lua'
-
-    --添加资源路径
-    cc.FileUtils:getInstance():addSearchPath("src/" .. newPath .. "/")
 
     --初始化游戏
     return true
