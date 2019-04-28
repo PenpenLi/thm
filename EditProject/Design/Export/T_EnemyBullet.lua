b9b05b07c05742e6d9d77e740615802d28694df0
cnscj <cnscj@qq.com>
2019-04-29 00:20:26 +0800

    fix:配置文件

---
+++ EditProject/Design/Export/T_EnemyBullet.lua
@@ -0,0 +1,29 @@
+return {
+	[6001001] = {
+		code = 6001001,
+		name = "大玉",
+		type = 1,
+		color = 1,
+		life = 100,
+		harm = 999,
+		atlas = "",
+	},
+	[6001002] = {
+		code = 6001002,
+		name = "小玉",
+		type = 1,
+		color = 1,
+		life = 100,
+		harm = 999,
+		atlas = "",
+	},
+	[6001003] = {
+		code = 6001003,
+		name = "螺旋玉",
+		type = 1,
+		color = 1,
+		life = 100,
+		harm = 999,
+		atlas = "",
+	},
+}
