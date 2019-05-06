4a33e976363a435d3e29aea88768e5d05825930b
cnscj <cnscj@qq.com>
2019-05-06 21:39:28 +0800

    fix:...

--- thm/src/app/th08/Scripts/Configs/Types/AnimStatus.lua
+++ thm/src/app/th08/Scripts/Configs/Types/AnimStatus.lua
@@ -5,5 +5,7 @@ DEFAULT = "default"             --默认动作
 IDLE = "idle"                   --闲置
 MOVE_LEFT = "move_left"         --向左移动
 MOVE_RIGHT = "move_right"       --向右移动
+ATTACK = "attack"
 
---八云蓝的特有动画
+--八云蓝的特有动作
+
